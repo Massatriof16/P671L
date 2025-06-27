7f3d7cd0bd75f96e2ddfd8ebd1672c9e1926dc8e
Massatrio16 <dimassetosatrio@gmail.com>
2025-06-27 17:59:14 +0700

    initian

---
+++ twrp_P671L.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+DEVICE_PATH := device/itel/P671LN
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+
+# Inherit from itel device
+$(call inherit-product, device/itel/P671LN/device.mk)
+
+PRODUCT_DEVICE := P671LN
+PRODUCT_NAME := twrp_P671LN
+PRODUCT_BRAND := itel
+PRODUCT_MODEL := P671LN
+PRODUCT_MANUFACTURER := itel
+
+PRODUCT_GMS_CLIENTID_BASE := android-itel
