e71bf50f7e64d2e7770aedd731817d32dca9abd8
Massatrio16 <dimassetosatrio@gmail.com>
2025-07-18 16:53:16 +0700

    try fix uevent

--- device.mk
+++ device.mk
@@ -24,13 +24,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-service \
     android.hardware.boot@1.2-service \
     vendor.sprd.hardware.boot@1.2-service
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
